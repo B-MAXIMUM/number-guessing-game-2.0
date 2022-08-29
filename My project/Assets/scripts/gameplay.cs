b4612a507be508e4e7a2558de8340782745272b7
B-MAXIMUM <bgmundy5223@gmail.com>
2022-08-29 11:30:16 -0700

    added code

--- My project/Assets/scripts/gameplay.cs
+++ My project/Assets/scripts/gameplay.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class gameplay : MonoBehaviour
 {
-    private int guess = 5;
-    private int minValue = 1;
-    private int maxValue = 10;
+    [SerializeField] private int guess;
+    [SerializeField] private int minValue;
+    [SerializeField] private int maxValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +18,26 @@ public class gameplay : MonoBehaviour
     {
         cum();
     }
+    public void OnPressHigher()
+    {
+            minValue = guess;
+            guess = (maxValue + minValue) / 2;
+    }
+    public void OnPressLower()
+    {
+            maxValue = guess;
+            guess = (maxValue +minValue) / 2;
+    }
+    public void NextGuess()
+    {
+
+    }
     void StartGame()
     {
         guess = 5;
         maxValue = 10;
         minValue = 1;
-
-        Debug.Log("welcome to the sexy number guessing game");
-        Debug.Log("the number range is from " + minValue + " to " +maxValue+ "");
-        Debug.Log("i will guess a number and you say if it is higher or lower then : " +guess+ "");
-        Debug.Log("push arrow keys depending on higher(up arrow) or lower(down arrow) and enter if correct");
+        
         maxValue = maxValue+1;
     }
     void cum()
@@ -36,13 +46,11 @@ public class gameplay : MonoBehaviour
         {
             minValue = guess;
             guess = (maxValue + minValue) / 2;
-            Debug.Log("is this it or is it higher or lower than: " + guess);
         }
         if(Input.GetKeyDown(KeyCode.DownArrow))
         {
             maxValue = guess;
             guess = (maxValue +minValue) / 2;
-            Debug.Log("is it higher or lower or am i right: " + guess);
         }
         if(Input.GetKeyDown(KeyCode.Return))
         {
